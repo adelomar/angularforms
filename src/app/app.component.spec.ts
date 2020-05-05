7876b6874f30ba77ed31328fd575d8c355b6ab39
Adel Omar Mohammed Zaki <adelomar1983@hotmail.com>
2020-05-05 14:08:01 +0300

    commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,22 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { CoreModule } from '@core';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    });
-    TestBed.compileComponents();
-  });
+      imports: [RouterTestingModule, TranslateModule.forRoot(), CoreModule],
+      declarations: [AppComponent],
+      providers: [],
+    }).compileComponents();
+  }));
 
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+  }), 30000);
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,66 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
+import { merge } from 'rxjs';
+import { filter, map, switchMap } from 'rxjs/operators';
+
+import { environment } from '@env/environment';
+import { Logger, untilDestroyed } from '@core';
+import { I18nService } from '@app/i18n';
+
+const log = new Logger('App');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  @ViewChild('f', {static: true}) signupForm: NgForm;
-  defaulQuestion = 'pet'
-  answer ='';
-  genders=['male','female'];
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: ''
-  }
-  submitted=  false;
+export class AppComponent implements OnInit, OnDestroy {
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title,
+    private translateService: TranslateService,
+    private i18nService: I18nService
+  ) {}
 
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    this.signupForm.form.patchValue({
-      userData: {
-        username: suggestedName
-      }
-    });
-    // this.signupForm.setValue({
-    //   userData: {
-    //     username: suggestedName,
-    //     email: ''
-    //   },
-    //   secret: 'pet',
-    //   questionAnswer: '',
-    //   gender: 'male',
-    // });
+  ngOnInit() {
+    // Setup logger
+    if (environment.production) {
+      Logger.enableProductionMode();
+    }
+
+    log.debug('init');
+
+    // Setup translations
+    this.i18nService.init(environment.defaultLanguage, environment.supportedLanguages);
+
+    const onNavigationEnd = this.router.events.pipe(filter((event) => event instanceof NavigationEnd));
+
+    // Change page title on navigation or language change, based on route data
+    merge(this.translateService.onLangChange, onNavigationEnd)
+      .pipe(
+        map(() => {
+          let route = this.activatedRoute;
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+          return route;
+        }),
+        filter((route) => route.outlet === 'primary'),
+        switchMap((route) => route.data),
+        untilDestroyed(this)
+      )
+      .subscribe((event) => {
+        const title = event.title;
+        if (title) {
+          this.titleService.setTitle(this.translateService.instant(title));
+        }
+      });
   }
 
-  onSubmit(){
-    this.submitted = true;
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.secretQuestion = this.signupForm.value.secret;
-    this.user.answer = this.signupForm.value.questionAnswer;
-    this.user.gender = this.signupForm.value.gender;
-    console.log(this.signupForm);
-    this.signupForm.reset();
+  ngOnDestroy() {
+    this.i18nService.destroy();
   }
-  // onSubmit(form: NgForm){
-  //   console.log(form.value);
-  // }
 }
